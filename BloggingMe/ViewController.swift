8626a5c3dbb8ad303ec5777dc3114a3cb9c3c2fb
Anton <stafeevt@mail.ru>
2022-10-06 15:57:30 +0500

    Initial Commit

---
+++ BloggingMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BloggingMe
+//
+//  Created by Антон Стафеев on 06.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
