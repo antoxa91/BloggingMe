9243c67ba883f09bcfc6527710defb33c7dc5216
Anton <stafeevt@mail.ru>
2022-10-13 17:31:36 +0500

    Upload Profile Pic

--- BloggingMe/Manager/StorageManager.swift
+++ BloggingMe/Manager/StorageManager.swift
@@ -12,16 +12,33 @@ final class StorageManager {
     
     static let shared = StorageManager()
     
-    private let container = Storage.storage().reference()
+    private let container = Storage.storage()
     
     private init() {}
     
     public func uploadUserProfilePicture(email: String, image: UIImage?, completion: @escaping (Bool) -> Void) {
+        let path = email
+            .replacingOccurrences(of: "@", with: "_")
+            .replacingOccurrences(of: ".", with: "_")
         
+        guard let jpegData = image?.jpegData(compressionQuality: 0.5) else { return }
+                
+        container
+            .reference(withPath: "profile_pictures/\(path)/photo.png")
+            .putData(jpegData) { metadata, error in
+                guard metadata != nil, error == nil else {
+                    completion(false)
+                    return
+                }
+                completion(true)
+            }
     }
     
-    public func downloadURLForProfilePicture(user: User, completion: @escaping (URL?) -> Void) {
-        
+    public func downloadURLForProfilePicture(path: String, completion: @escaping (URL?) -> Void) {
+        container.reference(withPath: path)
+            .downloadURL { url, _ in
+                completion(url)
+            }
     }
     
     public func uploadHeaderImage(blogPost: BlogPost, image: UIImage?, completion: @escaping (Bool) -> Void) {
