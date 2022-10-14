fccc780a49ef241cc078af8f7f74a97ab3eacb3e
Anton <stafeevt@mail.ru>
2022-10-14 17:42:55 +0500

    Making Start Screen

--- BloggingMe/Controllers/Posts/CreateNewPostViewController.swift
+++ BloggingMe/Controllers/Posts/CreateNewPostViewController.swift
@@ -9,7 +9,26 @@ import UIKit
 
 final class CreateNewPostViewController: UIViewController {
     
+    //Title field
+    //Image Header
+    //TextView Post
+    ///Возможно в отдельный хэдер вынести
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configureButtons()
+    }
+    
+    private func configureButtons() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .done, target: self, action: #selector(didTapCancel))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Post", style: .done, target: self, action: #selector(didTapPost))
+    }
+    
+    @objc private func didTapCancel() {
+        dismiss(animated: true)
+    }
+    
+    @objc private func didTapPost() {
+        //check data and post
     }
 }
