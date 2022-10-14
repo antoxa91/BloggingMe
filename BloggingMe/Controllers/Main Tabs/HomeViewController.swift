02b7b725d54d386fd0f97c8b8c40713af46c6f12
Anton <stafeevt@mail.ru>
2022-10-14 17:41:53 +0500

    Created Compose Button

--- BloggingMe/Controllers/Main Tabs/HomeViewController.swift
+++ BloggingMe/Controllers/Main Tabs/HomeViewController.swift
@@ -9,12 +9,50 @@ import UIKit
 
 final class HomeViewController: UIViewController {
     
+    private let composeButton: UIButton = {
+       let button = UIButton()
+        button.backgroundColor = .systemBlue
+        button.tintColor = .white
+        button.layer.shadowColor = UIColor.label.cgColor
+        button.layer.shadowOpacity = 0.4
+        button.layer.shadowRadius = 10
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .systemBackground
-        
-        
+        view.addSubview(composeButton)
+        composeButton.addTarget(self, action: #selector(didTapCreate), for: .touchUpInside)
+        setConstraints()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        composeButton.layer.cornerRadius = composeButton.frame.size.width/2
+        composeButton.setImage(
+            UIImage(systemName: "square.and.pencil", withConfiguration: UIImage.SymbolConfiguration(pointSize: view.frame.size.width/17, weight: .medium)), for: .normal)
+    }
+    
+    @objc private func didTapCreate() {
+        let vc = CreateNewPostViewController()
+        vc.title = "Create Post"
+        let navVC = UINavigationController(rootViewController: vc)
+        present(navVC, animated: true)
+    }
+}
+
+
+// MARK: - Constraints
+extension HomeViewController {
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            composeButton.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.15),
+            composeButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.15),
+            composeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            composeButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
+        ])
     }
 }
 
