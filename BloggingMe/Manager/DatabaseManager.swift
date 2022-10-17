c4f7529384fc751723ec14c7a5fc4857db5b1495
Anton <stafeevt@mail.ru>
2022-10-17 16:21:24 +0500

    filled insertPost() & getPosts() methods

--- BloggingMe/Manager/DatabaseManager.swift
+++ BloggingMe/Manager/DatabaseManager.swift
@@ -16,16 +16,69 @@ final class DatabaseManager {
     
     private init() {}
     
-    public func insertPost(blogPost: BlogPost, user: User, completion: @escaping(Bool) -> Void) {
-        
+    public func insertPost(blogPost: BlogPost, email: String, completion: @escaping(Bool) -> Void) {
+        let userEmail = email
+            .replacingOccurrences(of: ".", with: "_")
+            .replacingOccurrences(of: "@", with: "_")
+
+        let data: [String: Any] = [
+            "id": blogPost.identifier,
+            "title": blogPost.title,
+            "body": blogPost.text,
+            "created": blogPost.timestamp,
+            "headerImageURL": blogPost.headerImageUrl?.absoluteString ?? ""
+        ]
+
+        database
+            .collection("users")
+            .document(userEmail)
+            .collection("posts")
+            .document(blogPost.identifier)
+            .setData(data) { error in
+                completion(error == nil)
+            }
     }
     
     public func getAllPosts(completion: @escaping([BlogPost]) -> Void) {
-        
+ 
     }
     
-    public func getPosts(user: User, completion: @escaping([BlogPost]) -> Void) {
-        
+    public func getPosts(for email: String, completion: @escaping([BlogPost]) -> Void) {
+        let userEmail = email
+            .replacingOccurrences(of: ".", with: "_")
+            .replacingOccurrences(of: "@", with: "_")
+        database
+            .collection("users")
+            .document(userEmail)
+            .collection("posts")
+            .getDocuments { snapshot, error in
+                guard let documents = snapshot?.documents.compactMap({ $0.data() }),
+                      error == nil else {
+                    return
+                }
+
+                let posts: [BlogPost] = documents.compactMap({ dictionary in
+                    guard let id = dictionary["id"] as? String,
+                          let title = dictionary["title"] as? String,
+                          let body = dictionary["body"] as? String,
+                          let created = dictionary["created"] as? TimeInterval,
+                          let imageUrlString = dictionary["headerImageURL"] as? String else {
+                        print("Invalid post fetch conversion")
+                        return nil
+                    }
+
+                    let post = BlogPost(
+                        identifier: id,
+                        title: title,
+                        timestamp: created,
+                        headerImageUrl: URL(string: imageUrlString),
+                        text: body
+                    )
+                    return post
+                })
+
+                completion(posts)
+            }
     }
     
     public func insertUser(_ user: User, completion: @escaping(Bool) -> Void) {
@@ -60,8 +113,7 @@ final class DatabaseManager {
                     return
                 }
                 
-                var ref = data["profile_photo"]
-                
+                let ref = data["profile_photo"]
                 let user = User(name: name, email: email, profilePictureRef: ref)
                 completion(user)
             }
