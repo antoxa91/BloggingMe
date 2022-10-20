7b60f72c00f7c50520dfad68b57357ee186de322
Anton <stafeevt@mail.ru>
2022-10-20 17:40:10 +0500

    Custom Tab Bar

--- BloggingMe/Controllers/Main Tabs/TabBarController.swift
+++ BloggingMe/Controllers/Main Tabs/TabBarController.swift
@@ -11,6 +11,8 @@ final class TabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.additionalSafeAreaInsets.bottom = 15
+        setTabBarAppearance()
         setupControllers()
     }
     
@@ -20,18 +22,56 @@ final class TabBarController: UITabBarController {
         let home = HomeViewController()
         home.title = "Home"
         home.navigationItem.largeTitleDisplayMode = .always
+        let homeVC = UINavigationController(rootViewController: home)
+        homeVC.navigationBar.prefersLargeTitles = true
+        homeVC.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 1)
         
         let profile = ProfileViewController(currentEmail: currentUserEmail)
         profile.title = "Profile"
         profile.navigationItem.largeTitleDisplayMode = .always
+        let profileVC = UINavigationController(rootViewController: profile)
+        profileVC.navigationBar.prefersLargeTitles = true
+        profileVC.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.circle"), tag: 2)
         
-        let nav1 = UINavigationController(rootViewController: home)
-        nav1.navigationBar.prefersLargeTitles = true
-        nav1.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 1)
-        let nav2 = UINavigationController(rootViewController: profile)
-        nav2.navigationBar.prefersLargeTitles = true
-        nav2.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.circle"), tag: 2)
+        let createPost = CreateNewPostViewController()
+        createPost.title = "Create Post"
+        let createPostVC = UINavigationController(rootViewController: createPost)
+        createPostVC.tabBarItem = UITabBarItem(title: "Create Post", image: UIImage(systemName: "plus.circle"), tag: 3)
         
-        setViewControllers([nav1, nav2], animated: true)
+        setViewControllers([homeVC, createPostVC, profileVC], animated: true)
+    }
+    
+    private func setTabBarAppearance() {
+        let positionOnX: CGFloat = 10
+        let positionOnY: CGFloat = 12
+        let width = tabBar.bounds.width - positionOnX * 2
+        let height = tabBar.bounds.height + positionOnY * 2
+
+        let roundLayer = CAShapeLayer()
+        
+        let bezierPath = UIBezierPath(
+            roundedRect: CGRect(
+                x: positionOnX,
+                y: tabBar.bounds.minY - positionOnY,
+                width: width,
+                height: height
+            ),
+            cornerRadius: height / 2
+        )
+        roundLayer.path = bezierPath.cgPath
+        
+        tabBar.layer.insertSublayer(roundLayer, at: 0)
+        
+        tabBar.itemWidth = width / 5
+        tabBar.itemPositioning = .centered
+        
+        roundLayer.fillColor = UIColor(named: "CompButtonBackground")!.cgColor
+        roundLayer.shadowRadius = 5
+        roundLayer.shadowOpacity = 0.5
+        roundLayer.shadowColor = UIColor.blue.cgColor
+        roundLayer.shadowOffset = CGSize(width: 0, height: -2)
+
+        tabBar.tintColor = UIColor(named: "TabBarTint")
+        tabBar.unselectedItemTintColor = .systemGray
     }
 }
