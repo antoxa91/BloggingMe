2adcb2d057bae1539f15c58ac56a4830c93b9d22
Anton <stafeevt@mail.ru>
2022-10-21 15:34:47 +0500

    update

--- BloggingMe/Application/AppDelegate.swift
+++ BloggingMe/Application/AppDelegate.swift
@@ -13,11 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
-        
-        let appearance = UITabBarAppearance()
-        appearance.configureWithTransparentBackground()
-        UITabBar.appearance().standardAppearance = appearance
-        UITabBar.appearance().scrollEdgeAppearance = appearance
 
         return true
     }
