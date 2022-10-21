d65c0c8fbbbc5128e10e3ebbfbb8adf704024314
Anton <stafeevt@mail.ru>
2022-10-21 14:27:23 +0500

    TransparentBackground for UITabBar

--- BloggingMe/Application/AppDelegate.swift
+++ BloggingMe/Application/AppDelegate.swift
@@ -13,6 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
+        
+        let appearance = UITabBarAppearance()
+        appearance.configureWithTransparentBackground()
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+
         return true
     }
     
